04f3994218b925e4b2b8ec04d7a41d5d0290cd45
Jordan Navarro <jordan.navarro1@live.com>
2020-09-14 20:24:35 -0300

    Primeiro commit

---
+++ src/test/java/br/ielusc/natacao/NatacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ielusc.natacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
